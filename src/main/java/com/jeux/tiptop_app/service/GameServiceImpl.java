ad03577a97a08f1ac35a136b85ce8ec663b26697
ChaimaeFalih <chaimae.falih@ohm-energie.com>
2023-12-09 13:57:57 +0100

    test pipeline

--- src/main/java/com/jeux/tiptop_app/service/GameServiceImpl.java
+++ src/main/java/com/jeux/tiptop_app/service/GameServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService {
-//test pipelinetest
+//test pipelinetest3
     @Autowired
     GameRepository gameRepository;
 
